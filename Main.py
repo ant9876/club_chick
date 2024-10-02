cabfebecf2cb4254acf13362574f978fcee7d19b
ant9876 <anishkatrehan@gmail.com>
2024-10-02 08:40:17 -0400

    has a tree now :)

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 from doctest import master
-
+import os
 import pygame
 from pygame.draw_py import draw_polygon
 
@@ -12,12 +12,13 @@ clock = pygame.time.Clock()
 master_list = []
 
 
+
 class Player(pygame.Rect):
     def __init__(self):
         super().__init__(300, 300, 20, 20)
 
     def draw(self):
-        pygame.draw.rect(screen, "gray", self, 0)
+        pygame.draw.rect(screen, (245,228,156), self, 0)
 
 
 
@@ -32,8 +33,13 @@ class Object(pygame.Rect):
         self.y+=self.vy
 
 class Tree(Object):
+    def __init__(self, x, y, width, height, image_path):
+        super().__init__(x, y, width, height)
+        self.image = pygame.image.load(image_path)
+
     def draw(self):
-        pygame.draw.rect(screen, "green", self, 0)
+        screen.blit(self.image, (self.x, self.y))  # Draw the tree image
+
 
 class Rock(Object):
     def draw(self):
@@ -43,9 +49,9 @@ class Rock(Object):
 
 player = Player()
 
-
-master_list.append(Tree(500, 300, 10, 50))
-master_list.append(Tree(-50, 300, 10, 50))
+tree_one = os.path.expanduser("~/Downloads/pixil-frame-0 (2).png")
+master_list.append(Tree(500, 300, 10, 50, tree_one))
+master_list.append(Tree(-50, 300, 10, 50, tree_one))
 master_list.append(Rock(100,200,12,12))
 
 
@@ -56,30 +62,30 @@ while True:
             pygame.quit()
             raise SystemExit
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
+            if event.key == pygame.K_LEFT:
                 for obj in master_list:
                     obj.vx += 3
-            elif event.key == pygame.K_LEFT:
+            elif event.key == pygame.K_RIGHT:
                 for obj in master_list:
                     obj.vx -= 3
-            elif event.key == pygame.K_DOWN:
+            elif event.key == pygame.K_UP:
                 for obj in master_list:
                     obj.vy += 3
-            elif event.key == pygame.K_UP:
+            elif event.key == pygame.K_DOWN:
                 for obj in master_list:
                     obj.vy -= 3
 
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
+            if event.key == pygame.K_LEFT:
                 for obj in master_list:
                     obj.vx -= 3
-            elif event.key == pygame.K_LEFT:
+            elif event.key == pygame.K_RIGHT:
                 for obj in master_list:
                     obj.vx += 3
-            elif event.key == pygame.K_DOWN:
+            elif event.key == pygame.K_UP:
                 for obj in master_list:
                     obj.vy -= 3
-            elif event.key == pygame.K_UP:
+            elif event.key == pygame.K_DOWN:
                 for obj in master_list:
                     obj.vy += 3
 
@@ -88,7 +94,7 @@ while True:
 
     # Do logical updates here.
 
-    screen.fill("white")  # Fill the display with a solid color
+    screen.fill((123, 191, 98)) # Fill the display with a solid color
 
     # Render the graphics here.
     player.draw()
